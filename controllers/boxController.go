b8d21d3e481c5d82794788832aaa1fdf6f87dbed
tigerbeatle <tigerbeatle.yokosuka@gmail.com>
2019-02-02 11:41:44 -0700

    Added Downstream Comms

--- controllers/boxController.go
+++ controllers/boxController.go
@@ -96,7 +96,7 @@ func (c *BoxContext) UpdateBox(w http.ResponseWriter, r *http.Request){
 	basic := models.BasicJSONReturn{"UpdateBox", "200", string(jsonStr[:len(jsonStr)])}
 
 	s := fmt.Sprintf("%v", basic)
-	req, err := http.NewRequest("POST", dns.Parcel.Url, bytes.NewBuffer([]byte(s))
+	req, err := http.NewRequest("POST", dns.Parcel.Url, bytes.NewBuffer([]byte(s)))
 	if err != nil {
 		fmt.Println(err)
 		return
