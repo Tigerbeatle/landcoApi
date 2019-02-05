4eb921a7367f2c891b522de920834ec0a50573fe
tigerbeatle <tigerbeatle.yokosuka@gmail.com>
2019-02-05 12:38:19 -0700

    Added Transaction Model

---
+++ models/parcel.go
@@ -0,0 +1,20 @@
+package models
+
+import "github.com/mongodb/mongo-go-driver/mongo"
+
+type ParcelRepo struct {
+	Coll *mongo.Collection
+}
+
+type (
+	Parcel struct {
+		AccountUUID string	`json:"accountUUID"`
+		Name        string	`json:"name"`
+		Desc        string	`json:"desc"`
+		Owner       Person	`json:"owner"`
+		GroupUUID   string	`json:"groupUUID"`
+		Area        int		`json:"area"`
+		UUID		string	`json:"uuid"`
+		SeeAvatars	int		`json:"seeAvatars"`
+	}
+)
--- models/transaction.go
+++ models/transaction.go
@@ -14,10 +14,15 @@ type (
 		TransactionUUID string		`json:"transactionUUID"`
 		AccountUUID 	string		`json:"accountUUID"`
 		Amount			int			`json:"amount"`
+		Purpose			string		`json:"purpose"` // ex: rent payment, land purchase, back payment
 		SourceObject	string		`json:"sourceObject"` // what object created the transaction
 		CreatedAt 		time.Time	`json:"created_at"`
 		UpdatedAt 		time.Time	`json:"updated_at"`
 		Description 	string		`json:"description"`
 		Payee 			Person		`json:"payee"` // The person who made the payment
+		ParcelUUID		string		`json:"parcelUUID"`
+		ResultUUID		string		`json:"resultUUID"` // transaction result key returned by llTransferLindenDollars
 	}
 )
+
+
